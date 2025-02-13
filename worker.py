61919275dffbdb497f15cd1f695bda7356d5a81a
Zyphron-Vexmore <jotiba1312@gmail.com>
2025-02-13 11:54:33 +0530

    Update worker.py
    Signed-off-by: Zyphron-Vexmore <jotiba1312@gmail.com>

--- worker.py
+++ worker.py
@@ -8,8 +8,7 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from seleniumwire import webdriver as wire_webdriver  # Import selenium-wire webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
-
+import chromedriver_autoinstaller
 
 # Proxy configuration
 PROXY_HOSTS = [
